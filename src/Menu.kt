7261e89a759073b50d08eb2024cd7cb0eb693698
max6811 <maxc.m6811@gmail.com>
2020-05-08 16:45:42 -0400

    functon for options selected

--- src/Menu.kt
+++ src/Menu.kt
@@ -10,15 +10,43 @@ fun main(args:Array<String>){
         2. Ver mis recetas
         3. Salir
     """.trimIndent()
-    do {
+    stopLoop@ do {
         println(options)
         val response: String? = readLine()
         repit@ when( response?.toInt()){
-            1 -> println("Listos para hacer la receta, vamos!")
-            2 -> println("Veamos las recetas")
+            1 -> {
+//                    println("Listos para hacer la receta, vamos!")
+                    repicesMaker()
+                    break@stopLoop
+                }
+            2 -> {
+//                    println("Veamos las recetas")
+                    viewRepices()
+                    break@stopLoop
+                }
             3 -> println("Vuelve pronto")
             else -> println("Debe ingresar una de las opciones")
 
         }
     }while (response?.toInt() != 3)
+}
+
+fun repicesMaker(){
+    val options: String = """
+        Hacer receta
+        Selecciona por categoria el ingrediente que buscas
+        1. Agua
+        2. Leche
+        3. Carne
+        4. Verduras
+        5. Frutas
+        6. Cereal
+        7. Huevos
+        8. Aceites
+    """.trimIndent()
+    println(options)
+}
+fun viewRepices(){
+    val title: String = "Ver mis recetas"
+    println(title)
 }
